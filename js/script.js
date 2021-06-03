9e8bf2a7780409d69b33db044c948402a41d5dc0
dziubacaique <caiquedziuba@hotmail.com>
2021-06-03 18:16:20 -0300

    Tratando movimentos ao chegar nas laterais da tela

--- js/script.js
+++ js/script.js
@@ -8,11 +8,13 @@ snake[0] = {
 }
 let direction = "right";
 
+/*Função criar o background do jogo*/
 function criarBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+/*Função criar a cobra no background do jogo*/
 function criarCobra() {
     for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
@@ -20,13 +22,48 @@ function criarCobra() {
     }
 }
 
+
+document.addEventListener('keydown', update);/*Pegar o evento de clique e chamar a função update*/
+
+/*configura as teclas e trata para a cobra não andar na direção oposta que está atualmente*/
+function update(event){
+    if (event.keyCode == 37 && direction != "right"){
+        direction = "left";
+    }
+    if (event.keyCode == 38 && direction != "down"){
+        direction = "up";
+    }
+    if (event.keyCode == 39 && direction != "left"){
+        direction = "right";
+    }
+    if (event.keyCode == 40 && direction != "up"){
+        direction = "down";
+    }
+}
+
+/*Função de iniciar o jogo*/
 function startGame() {
+    /*tratando para quando a cobra chegar em um limite lateral, ela vá para o lado oposto*/
+    if(snake[0].x > 15 * box && direction == "right") {
+        snake[0].x = 0;
+    }
+    if(snake[0].x < 0 * box && direction == "left") {
+        snake[0].x = 16 * box;
+    }
+    if(snake[0].y > 15 * box && direction == "down") {
+        snake[0].y = 0;
+    }
+    if(snake[0].y < 0 * box && direction == "up") {
+        snake[0].y = 16 * box;
+    }
+
     criarBG(); /* criando o background da tela do jogo*/
     criarCobra(); /*cria a cobrinha na tela*/
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
+    /*Condicionais de movimentação da cobra*/
     if (direction == "right") {
         snakeX += box;
     }
